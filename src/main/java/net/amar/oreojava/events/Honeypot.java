0889a70338545ae2021d37e770a1833a391fc69c
AmarKherala <markhyrallh6@gmail.com>
2026-02-21 03:25:03 +0300

    update Honeypot.java to timeout for 1 day

--- src/main/java/net/amar/oreojava/events/Honeypot.java
+++ src/main/java/net/amar/oreojava/events/Honeypot.java
@@ -3,41 +3,69 @@ package net.amar.oreojava.events;
 import net.amar.oreojava.Log;
 import net.amar.oreojava.Oreo;
 import net.amar.oreojava.db.tables.Case;
+import net.amar.oreojava.handlers.ParseMute;
 import net.amar.oreojava.handlers.Verdict;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.concurrent.TimeUnit;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Honeypot extends ListenerAdapter {
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         if (event.getAuthor().isBot()) return;
-        if (Oreo.getForbiddenChannel()==null) {
+        if (!event.isFromGuild()) return;
+
+        if (Oreo.getForbiddenChannel() == null) {
             Log.warn("honeypot isn't set");
             return;
         }
-        String channelId = event.getChannel().getId();
 
-        if (channelId.equals(Oreo.getForbiddenChannel().getId())) {
-            event.getMessage().delete().reason("honeypot").queue();
-            User u = event.getAuthor();
-            Case c = new Case(
-                    u.getId(),
-                    u.getName(),
-                    event.getJDA().getSelfUser().getId(),
-                    event.getJDA().getSelfUser().getName(),
-                    "KICK",
-                    "Fell in the honeypot",
-                    "",
-                    true);
-            if (Verdict.buildVerdict(c, Oreo.getVerdictChannel(), u, null))
-                Log.info(u.getName()+" tried to send a message in forbidden channel");
-            event.getGuild().ban(u,1, TimeUnit.DAYS)
-                    .queue(success -> event.getGuild().unban(u).queue());
+        TextChannel channel = event.getChannel().asTextChannel();
 
-        }
+        if (!channel.getId().equals(Oreo.getForbiddenChannel().getId()))
+            return;
+
+        User user = event.getAuthor();
+        OffsetDateTime cutoff = OffsetDateTime.now().minusMinutes(15);
+
+        channel.getHistory().retrievePast(100).queue(messages -> {
+
+            List<Message> toDelete = new ArrayList<>();
+
+            for (Message msg : messages) {
+                if (msg.getTimeCreated().isBefore(cutoff))
+                    break;
+
+                if (msg.getAuthor().getIdLong() == user.getIdLong())
+                    toDelete.add(msg);
+            }
+
+            if (!toDelete.isEmpty())
+                channel.purgeMessages(toDelete);
+        });
+
+        event.getMessage().delete().reason("honeypot").queue();
+
+        Case c = new Case(
+                user.getId(),
+                user.getName(),
+                event.getJDA().getSelfUser().getId(),
+                event.getJDA().getSelfUser().getName(),
+                "MUTE",
+                "Fell in the honeypot",
+                "1 day",
+                true
+        );
+
+        if (Verdict.buildVerdict(c, Oreo.getVerdictChannel(), user, null))
+            Log.info(user.getName() + " tried to send a message in forbidden channel");
+        ParseMute.mute(user, 1, "d", event.getGuild(), "honeypot");
     }
 }
